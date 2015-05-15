48794fa120b353501b0564841e31b25756d5aa10
Priyanka Jain <stunningpriyanka19@gmail.com>
2015-05-15 15:56:15 +0530

    Show list price and cost price of products on variants level #7744
    Description: Created a new module to show list price and cost price of products on variants level.
    
    IssueID: 10903002

--- __init__.py
+++ __init__.py
@@ -6,9 +6,12 @@
     :license: BSD, see LICENSE for more details.
 """
 from trytond.pool import Pool
+from product import Template, Product
 
 
 def register():
     Pool.register(
+        Template,
+        Product,
         module='product_variant', type_='model'
     )
